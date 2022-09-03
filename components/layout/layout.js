70c3e2e373b120beb5b57f4a5ba309f626e72282
Jerome Fabricante <jerome.fabricante@opsolutions.biz>
2022-09-03 11:10:16 +0800

    Event manager web app

---
+++ components/layout/layout.js
@@ -0,0 +1,11 @@
+import { Fragment } from "react";
+import Mainheader from "./main-header";
+
+export default function Layout(props) {
+  return (
+    <Fragment>
+      <Mainheader />
+      <main>{props.children}</main>
+    </Fragment>
+  );
+}
---
+++ pages/events/[...slug].js
@@ -0,0 +1,58 @@
+import { useRouter } from "next/router";
+import { Fragment } from "react";
+import EventList from "../../components/events/event-list";
+import ResultsTitle from "../../components/events/results-title";
+import Button from "../../components/ui/button";
+import ErrorAlert from "../../components/ui/error-alert";
+import { getFilteredEvents } from "../../dummy-data";
+
+export default function FilteredEventsPage() {
+  const router = useRouter();
+
+  const filterData = router.query.slug;
+
+  if (!filterData) {
+    return <p className="center">Loading...</p>;
+  }
+
+  const [filteredYear, filteredMonth] = filterData;
+  const numYear = +filteredYear;
+  const numMonth = +filteredMonth;
+
+  if (isNaN(numYear) || isNaN(numMonth)) {
+    return (
+      <Fragment>
+        <ErrorAlert>
+          <p>Invalid filter!</p>
+        </ErrorAlert>
+        <div className="center">
+          <Button link="/events">Show All Events</Button>
+        </div>
+      </Fragment>
+    );
+  }
+
+  const filteredEvents = getFilteredEvents({ year: numYear, month: numMonth });
+
+  if (!filteredEvents || filteredEvents.length === 0) {
+    return (
+      <Fragment>
+        <ErrorAlert>
+          <p>No event found for the chosen filter!</p>
+        </ErrorAlert>
+        <div className="center">
+          <Button link="/events">Show All Events</Button>
+        </div>
+      </Fragment>
+    );
+  }
+
+  const date = new Date(numYear, numMonth - 1);
+
+  return (
+    <Fragment>
+      <ResultsTitle date={date} />
+      <EventList items={filteredEvents} />
+    </Fragment>
+  );
+}
